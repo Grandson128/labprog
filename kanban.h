b08a38b7a94349f8f835b96efb8375109d8c1d10
Grandson <up201407729@fc.up.pt>
2021-03-11 15:23:45 +0000

    +Structures for Taks, TaskList, Date and Person +Functions file (Ex: manipulate lists)

--- kanban.h
+++ kanban.h
@@ -1,1 +1,70 @@
-#kanban library
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * 
+ *  Structures Declaration  
+ * 
+ ***/
+
+typedef struct node* Tasklist;
+
+typedef struct task {
+    int id;
+    int priority; 
+    int state; // 1- ToDo; 2- Doing; 3- Done
+    char *description; 
+    Date creationDate;
+    Date targetDate;
+    Date finalDate;
+    Slave person;
+}Task ;
+
+typedef struct node{
+    int info;
+    Task *Task;
+    Tasklist next;
+}Node;
+
+typedef struct {
+    int day;
+    int month;
+    int year;
+}Date;
+
+typedef struct{
+    int id;
+    char *name;
+}Person;
+
+
+
+/**
+ *
+ *  
+ * Functions Declaration 
+ * 
+ * 
+ **/
+
+Tasklist createTaskList();
+Tasklist removeTaskList (Tasklist list);
+int emptyList (Tasklist list);
+int fullList (Tasklist list);
+void insertItemList (Tasklist list, Task task);
+void deletItemList (Tasklist list, int item);
+
+Tasklist searchList (Tasklist list, Task task); //Using task to search
+Tasklist searchList (Tasklist list, int task); //Using task Identifier to search
+
+void printlist (Tasklist lista);
+
+
+
+
+
+#endif
--- main.c
+++ main.c
@@ -1,1 +1,10 @@
-#main code file
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("O nosso projeto em C para Laboratorio de Programacao");
+    
+
+    return 0;
+}
