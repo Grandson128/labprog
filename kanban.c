4958f04fb6806bf10e5a3874d347281bbe27407e
grandson128 <up201407729@fc.up.pt>
2021-03-11 15:53:09 +0000

    +createTaskList +removeTaskList

--- kanban.c
+++ kanban.c
@@ -3,3 +3,26 @@
 #include "kanban.h"
 
 
+
+Tasklist createTaskList(){
+    Tasklist aux;
+    aux = (Tasklist)malloc(sizeof(Node));
+    if (aux != NULL) {
+        aux->info = 0;
+        aux->next = NULL;
+    }
+    return aux;
+
+}
+
+Tasklist removeTaskList (Tasklist list){
+    Tasklist temp_ptr;
+
+    while (lista_vazia(list) == 0){
+        temp_ptr = list;
+        list = list->next;
+        free(temp_ptr);
+    }
+    free(list);
+    return NULL;
+}
