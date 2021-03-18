9b33d3b86fd494db99094993a566b4de7a878ec8
josefernandes99 <41640094+josefernandes99@users.noreply.github.com>
2021-03-18 16:29:36 +0000

    Update kanban.c
    primeiro contributo do jovem prodígio josé fernandes

--- kanban.c
+++ kanban.c
@@ -138,3 +138,23 @@ Date change_date(Date date) { //é preciso
 Date validate_date(Date date){
 
 }
+
+/**
+ *
+ * Compares if date1 is after date2
+ *
+ */
+int compare_date(Date date1, Date date2){
+	if ((date1)->year > (date2)->year){
+		return 1;
+	}
+	else if ((date1)->year == (date2)->year && (date1)->month > (date2)->month){
+		return 1;
+	}
+	else if ((date1)->year == (date2)->year && (date1)->month == (date2)->month && (date1)->day > (date2)->day){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
